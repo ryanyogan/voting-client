8aab4b54f6103840e313740b50380798a4727d97
Ryan Yogan <ryanyogan@gmail.com>
2015-11-29 12:09:11 -0600

    trashed the hard coded temp props

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { List, Map } from 'immutable';
 
-const pair = List.of('Batman', 'Ants');
-const tally = Map({'Batman': 5, 'Ants': 1});
-
 export default class App extends React.Component {
   render() {
-    return React.cloneElement(this.props.children, {pair: pair, tally: tally});
+    return this.props.children;
   }
 }
